fb071f74dd7e8e15421557bc41dc09c1fb36f64e
Shameel Kadannamanna <shameelsadaka@gmail.com>
2019-08-12 01:36:19 +0530

    UI Ready

---
+++ script.js
@@ -0,0 +1,18 @@
+function renderCanvas(){
+    
+    var icon = document.getElementById("icon").value;
+    var district = document.getElementById("district").value;
+    var area = document.getElementById("area").value;
+    var message = document.getElementById("message").value;
+    var timestamp = document.getElementById("timestamp").value;
+    var name1 = document.getElementById("name1").value;
+    var contact1 = document.getElementById("contact1").value;
+    var name2 = document.getElementById("name2").value;
+    var contact2 = document.getElementById("contact2").value;
+
+    console.log(icon,district)
+
+
+
+
+}
